69465d80c05775ce2bf49c92b9cff77613d60a6b
zacharymcclure <mcc.zack93@gmail.com>
2021-07-17 10:57:20 -0600

    player can now perform a single jump when grounded

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private LayerMask platformsLayerMask;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpSpeed;
 
-    private void Start()
+    private BoxCollider2D boxCollider2d;
+
+    private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        boxCollider2d = transform.GetComponent<BoxCollider2D>();
+        moveSpeed = 5f;
+        jumpSpeed = 5f;
     }
 
     private void Update()
     {
         Move();
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
     }
 
     private void Move()
@@ -22,5 +33,17 @@ public class PlayerController : MonoBehaviour
         float moveX = Input.GetAxis("Horizontal");
         float moveBy = moveX * moveSpeed;
         rb.velocity = new Vector2(moveBy, rb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        rb.velocity = Vector2.up * jumpSpeed;
+    }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, .1f, platformsLayerMask);
+        Debug.Log(raycastHit2d.collider);
+        return raycastHit2d.collider != null;
     }
 }
